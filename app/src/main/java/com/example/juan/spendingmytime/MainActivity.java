a38605202307e78a7aa8527ec66daa6787942e0a
Juan Pablo Rabino <jprabino@gmail.com>
2016-09-03 23:46:06 -0300

    build on vaio

--- app/src/main/java/com/example/juan/spendingmytime/MainActivity.java
+++ app/src/main/java/com/example/juan/spendingmytime/MainActivity.java
@@ -1,31 +1,14 @@
 package com.example.juan.spendingmytime;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends AppCompatActivity {
-
-    public final static String EXTRA_MESSAGE = "com.example.juan.spendingmytime.EXTRA_MESSAGE";
-
-    startClockListener startlistener = new StartClockBtnListener();
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 
 }
